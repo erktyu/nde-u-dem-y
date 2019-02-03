55bd37081480d3d628b81f99a29e98ea3b700891
aga <a@a.a>
2019-02-03 08:34:09 +0100

    (S3L9-feature: fs[module], os[module]) get user info and register to txt file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('let\'s start');
+//section 3 - lesson 9
+
+const fs = require('fs');
+const os = require('os');
+
+//show the info of the user who is logged in atm.
+var user = os.userInfo();
+console.log(user);
+console.log(user.username); //etc..
+
+//create or edit a txt file called greetings.txt by adding a Helloword inside.
+fs.appendFile('greetings.txt',`Hello ${user.username} your shell is ${user.shell}\n` , function(err){
+    if(err){
+        console.log('Unable to write to file');
+    }
+})
