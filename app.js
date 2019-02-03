353659e49afc34f30e98074b9c953c629f3901c8
aga <a@a.a>
2019-02-03 15:58:31 +0100

    (S3L12-feature: lodash[npm module]) install new module from npm and us it.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-/**Section 3 - lesson 10
+/**Section 3 - lesson 11
  * 
- * Implementing local js files into require function
+ * npm initializing / creating package.json
+ * and 
+ * downloading lodash package "_"
+ * 
+ * TIPS: YOU CAN REMOVE WHOLE node_modules map and install all them back by 'npm install'
+ * in terminal. npm will find all packages with right versions through package.json file
  * 
  *----------------------------------------------------*/
 
 console.log('starting apps.js');
 
-//show the info of the user who is logged in atm
-const notes = require('./notes');
+const _ = require('lodash');
 
-//(ref.nr. 1A [see notes.js])use the variable that is exported 
-console.log(notes.age);
-const res = notes.addNote();
-console.log(res);
+console.log(_.isString(true));
+//shows false , because true is boolean not string
 
-//(ref.nr. 2A [see notes.js])use the variable that is exported 
-console.log(notes.add(5,10));
+console.log(_.isString(5));
+//shows false , because 5 is not string
+
+console.log(_.isString("5"));
+//shows true , because 5 is string
+
+console.log(_.uniq([1,1,1,1,1,2,3,3,3,"abc",'abc','abc','c','c']));
+//shows [ 1, 2, 3, 'abc', 'c' ]
